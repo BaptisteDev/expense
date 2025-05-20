df7bee36b1faa9859dc7af1ea3adca054fe400a4
BaptisteDev <dev.baptiste15@gmail.com>
2025-05-20 11:46:44 +0200

    TKt

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-const express = require("express");
+import express from "express";
+
 const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./database/config");
