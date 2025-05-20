13900466f1b86f04652b8f853f4becbd1e8665a7
BaptisteDev <dev.baptiste15@gmail.com>
2025-05-20 11:14:51 +0200

    TKt

---
+++ server/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./database/config");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+const routes = require("./routes");
+const { generalLimiter } = require("./middlewares/rateLimitMiddleware");
+
+app.use(generalLimiter);
+
+app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+mongoose
+  .connect(config.mongoDb.uri)
+  .then(() => {
+    console.log("Connexion Mongo DB OK");
+  })
+  .catch((err) => console.log(err));
+
+app.listen(3000);
+
+// localhost:3000
